52a00501b3bb2fbbef975a6c48dd68571d0e025a
Frank Mochoge <mochogefrank01@gmail.com>
2019-07-29 02:21:33 +0300

    Added DB settings and requirements.txt

--- vms/vms/settings.py
+++ vms/vms/settings.py
@@ -73,13 +73,21 @@ WSGI_APPLICATION = 'vms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vmsdb',
+        'USER': 'root',
+        'PASSWORD': 'vmsroot',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
